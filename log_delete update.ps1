2045e149e0099cba9e0b982d39b2b4311b2c187c
savex123 <savex123@naver.com>
2023-06-14 14:44:34 +0900

    first upload

---
+++ log_delete update.ps1
@@ -0,0 +1,9 @@
+﻿$logday =1;
+$creationDate = (Get-Date).AddDays($logday);
+$sorce1 = "C:\Users\gahyek\Desktop\Kimgahye 개인폴더\CF\test01\";
+$fordernamepattern = @("HSS.*","*.HSK","*.HSM","*.HSU","*.trc")
+#$sorce2 = "C:\Users\gahyek\Desktop\Kimgahye 개인폴더\CF\test01\HSS\";
+#$arrPath = @($sorce1,$sorce2);
+
+$blnRecurse = $ture;
+Get-ChildItem -path $sorce1 -Exclude $fordernamepattern -Recurse:$blnRecurse | where-Object { $_.LastWritetime -lt $creationDate } |  Remove-Item  -force
---
+++ log_ftpserver.ps1
@@ -0,0 +1,49 @@
+﻿#디버깅 경로
+$debugLogPath = "C:\CF_LOG_TEST\DebugLog.txt"
+
+# 디버깅 로그를 출력하는 함수
+function Write-DebugLog {
+    param([string]$message)
+
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logEntry = "$timestamp - $message"
+
+    # 로그를 파일에 추가
+    $logEntry | Out-File -FilePath $debugLogPath -Append
+
+    # 로그를 콘솔에 출력
+    Write-Host $logEntr
+}
+
+# 압축할 폴더 경로
+$folderPath = "C:\CF_LOG_TEST\HSS"
+
+# 압축 파일 경로
+$zipFilePath = "C:\CF_LOG_TEST\ftp.zip"
+
+# FTP 서버 정보
+$ftpServer = "220.95.208.243"
+$ftpUsername = "cf_ph"
+$ftpPassword = "c1p$Afr#=5Z3V-#9OS4l"
+$ftpRemotePath = "/Upload/08_Logs/SGP/QA/"
+
+# 폴더 압축
+Add-Type -A 'System.IO.Compression.FileSystem'
+[System.IO.Compression.ZipFile]::CreateFromDirectory($folderPath, $zipFilePath)
+
+# 업로드할 파일 경로
+$filePath = "C:\CF_LOG_TEST\ftp_test.zip"
+
+# FTP로 전송
+$ftpRequest = [System.Net.FtpWebRequest]::Create("ftp://$ftpServer$ftpRemotePath" + [System.IO.Path]::GetFileName($zipFilePath))
+$ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
+$ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+$ftpRequest.UseBinary = $true
+
+$fileContents = [System.IO.File]::ReadAllBytes($zipFilePath)
+
+# 압축 파일 삭제
+#Remove-Item $zipFilePath
+
+Write-Host "파일 전송이 완료되었습니다."
+
